b9f8b63e48ed0b215d8a4ccf043a43325910f467
7usseiny-eg <64532906+7usseiny-eg@users.noreply.github.com>
2021-06-05 19:38:19 +0200

    Update SEVEN_SEGMENT_DISPLAY.h

--- inc/SEVEN_SEGMENT_DISPLAY.h
+++ inc/SEVEN_SEGMENT_DISPLAY.h
@@ -1,1 +1,15 @@
-//test
+#ifndef seven_segment_display_H_
+#define seven_segment_display_H_
+
+#include "STD_TYPES.h"
+
+//To choose which display of the three will work
+void vDISPLAY_ENABLE(uint8 DISNUM);
+void vDISPLAY_DISABLE(uint8 DISNUM);
+//To choose which digit to appear from 0-9
+void vDIGIT_NUMBER(uint8 NUMBER);
+//INSERT A NUMBER TO DISPLAY ON LED
+void vLED_WRITE(uint16 NUM);
+
+
+#endif
