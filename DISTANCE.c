989533eac13c9a32abd2f8ec66840cab52126536
shawky20 <67594814+shawky20@users.noreply.github.com>
2021-06-05 16:26:40 +0200

    Update DISTANCE.c

--- DISTANCE.c
+++ DISTANCE.c
@@ -15,3 +15,16 @@ double rad_to_deg(double rad) { // CHANGE RADIAN TO DEGREE
 //FUNCTION RETURNS DISTANCE IN METRES
 //IMPORTANT: HAS TO BE CASTED
 
+double calc_Distance(double lat1, double lon1, double lat2, double lon2) {
+  double diff, dist;
+  if ((lat1 == lat2) && (lon1 == lon2)) {
+    return 0;
+  }
+  else {
+    diff = lon1 - lon2;
+    dist = (sin(deg_to_rad(lat1)) * sin(deg_to_rad(lat2))) + (cos(deg_to_rad(lat1)) * cos(deg_to_rad(lat2)) * cos(deg_to_rad(diff)));
+    dist = acos(dist) * EARTH_RADIUS * 1000;
+    return dist;
+  }
+}
+
