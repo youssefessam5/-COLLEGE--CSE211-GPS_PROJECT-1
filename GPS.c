8b91e6543b1ed9c32f42e3d71391ebbc261de066
omarosamaa <84813775+omarosamaa@users.noreply.github.com>
2021-06-14 03:32:45 +0200

    Update GPS.c

--- GPS.c
+++ GPS.c
@@ -10,3 +10,15 @@ uint8* u8GPS_GETLINE(void){
 	}
 	return line;
 }
+uint8* u8GPS_GETDATA(void){
+	int i;
+	uint8* line;
+	do{
+		line = u8GPS_GETLINE();
+	}while(strncmp(line, "$GPGLL", 6) != 0);
+	for(i = 0; i < 82; i++){
+		if(line[i] == '$' && i != 0)
+			line[i] = '\0';
+	}
+	return line;
+}
