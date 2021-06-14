34e4c557cfaefb365ab74c7fdd8694ea2e6c82f6
Youssef Essam <84783321+youssefessam5@users.noreply.github.com>
2021-06-14 03:09:41 +0200

    Update GPS.c

--- GPS.c
+++ GPS.c
@@ -1,13 +1,1 @@
-#include "inc/STD_TYPES.h"
-#include "inc/UART.h"
-#include <string.h>
-
-uint8* u8GPS_GETLINE(void){
-	int i ;
-	uint8 line[82];
-	for( i = 0; i < 82; i++){
-			line[i] = u8UART_Receive();
-	}
-	return line;
-}
 
