bb599ae30d419576e344f48079cbc050e39a8d07
shawky20 <67594814+shawky20@users.noreply.github.com>
2021-06-14 03:25:10 +0200

    Update LCD.h

--- inc/LCD.h
+++ inc/LCD.h
@@ -12,4 +12,14 @@
 #define LCD5x7					0x38
 
 
+void vLCD_NUMSTRING(uint8* str, uint8 amount, uint16 number);
+void vLCD_FIRSTLINE(uint8* str, uint8 amount, uint16 number);
+void vLCD_SECONDLINE(uint8* str, uint8 amount, uint16 number);
+void vLCD_INT (void);  									//LCD INITIALIZATIONS
+void vLCD_CMD(uint8 LCD_COMMAND );						//LCD COMMANDS
+void vLCD_DATA(uint8 DATA);	                            //LCD DATA
+void vLCD_STRING(uint8* str, uint8 amount);					        // LCD STRING OUTPUT
+void DELAY_MILLI(int n);                                // DELAY MILLIS
+void DELAY_MICRO(int n);                                // DELAY MICROS
 
+#endif
